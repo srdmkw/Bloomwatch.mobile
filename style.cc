2011963674273aa522cb0cc031a236a454a051e0
srdmkw <sardorazizbekovich@gmail.com>
2025-10-04 16:39:35 +0500

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,17 @@
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  background: #f5f8f5;
+}
+
+h1 {
+  margin: 10px 0;
+  color: #2e7d32;
+}
+
+#map {
+  height: 90vh;
+  width: 100%;
+  border-top: 2px solid #2e7d32;
+}
